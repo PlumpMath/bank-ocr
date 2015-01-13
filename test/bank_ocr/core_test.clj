267ea2e2c3bc2fa17361e4447bbef7cf1bc38d6a
Edward Cho <zerokarmaleft@gmail.com>
2015-01-13 14:58:50 -0600

    parse the rest of the digits

--- test/bank_ocr/core_test.clj
+++ test/bank_ocr/core_test.clj
@@ -10,4 +10,36 @@
   (testing "should parse 1"
     (is (= (parse-digit ["   "
                          "  |"
-                         "  |"]) 1))))
+                         "  |"]) 1)))
+  (testing "should parse 2"
+    (is (= (parse-digit [" _ "
+                         " _|"
+                         "|_ "]) 2)))
+  (testing "should parse 3"
+    (is (= (parse-digit [" _ "
+                         " _|"
+                         " _|"]) 3)))
+  (testing "should parse 4"
+    (is (= (parse-digit ["   "
+                         "|_|"
+                         "  |"]) 4)))
+  (testing "should parse 5"
+    (is (= (parse-digit [" _ "
+                         "|_ "
+                         " _|"]) 5)))
+  (testing "should parse 6"
+    (is (= (parse-digit [" _ "
+                         "|_ "
+                         "|_|"]) 6)))
+  (testing "should parse 7"
+    (is (= (parse-digit [" _ "
+                         "  |"
+                         "  |"]) 7)))
+  (testing "should parse 8"
+    (is (= (parse-digit [" _ "
+                         "|_|"
+                         "|_|"]) 8)))
+  (testing "should parse 9"
+    (is (= (parse-digit [" _ "
+                         "|_|"
+                         " _|"]) 9))))
