8b80614fdcfce9675a779a34c40d4f7265d556e0
Edward Cho <zerokarmaleft@gmail.com>
2015-01-14 23:14:40 -0600

    fix user story #1's sample run

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -3,10 +3,25 @@
             [bank-ocr.account :as account])
   (:gen-class))
 
-(defn -main
+(defn generate-report
+  "Write a report to out-file from parsing in-file, showing
+  conversions and annotations"
   [in-file out-file]
   (let [accounts (account/parse-file in-file)]
     (with-open [w (writer out-file)]
       (doseq [account accounts]
         (.write w (account/print-account-number account))
         (.write w "\n")))))
+
+(defn print-accounts
+  "Read and parse in-file, and display the conversion results"
+  [in-file]
+  (let [accounts (account/parse-file in-file)]
+    (doseq [account accounts]
+      (println (apply str account)))))
+
+(defn -main
+  ([in-file]
+     (print-accounts in-file))
+  ([in-file out-file]
+     (generate-report in-file out-file)))
